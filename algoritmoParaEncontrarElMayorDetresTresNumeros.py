81b38b5ee2f6ce10815d44ee3def9514b62c7ccc
Tu Nombre <you@example.com>
2024-02-23 10:40:33 -0500

    archovo del algoritmo de mayor

---
+++ algoritmoParaEncontrarElMayorDetresTresNumeros.py
@@ -0,0 +1,10 @@
+number1 = int(input("Ingrese el numero: "))
+number2 = int(input("Ingrese el numero: "))
+number3 = int(input("Ingrese el numero: "))
+
+if number2 < number1 > number3:
+    print("El numero mayor es el primer numero:",number1)
+elif number1 < number2 > number3:
+    print( "El numero mayor es el segundo numero: ",number2)
+elif number1 < number3 > number2:
+    print( "El numero mayor es el trecer numero: ",number3)
