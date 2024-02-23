c0d6967608b6822e7fe88739de2b686e6a9e8ae1
Sevasthian <0160695272+Sevasthian@users.noreply.github.com>
2024-02-23 14:03:31 -0500

    ejercicio de mayor

--- algoritmoParaEncontrarElMayorDetresTresNumeros.py
+++ algoritmoParaEncontrarElMayorDetresTresNumeros.py
@@ -8,3 +8,14 @@ elif number1 < number2 > number3:
     print( "El numero mayor es el segundo numero: ",number2)
 elif number1 < number3 > number2:
     print( "El numero mayor es el trecer numero: ",number3)
+
+elif number1 == number2  > number3 :
+    print("El numero mayor es el primer numero:",number2) 
+
+elif number1 == number3  > number2 :
+    print("El numero mayor es el primer numero:",number1)
+elif number2 == number3  > number1 :
+    print("El numero mayor es el primer numero:",number3)
+elif number1 == number2  == number3 :
+    print("El numero mayor es el primer numero:",number2)
+    
